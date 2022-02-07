08cc6a7e757b846b53fd2618034193810b94bc2a
ALinuxPerson <alinuxperson@gmail.com>
2022-02-07 13:39:02 +0800

    Remove old drop strategies

--- drop-strategies/noop/examples/main.rs
+++
@@ -1,9 +0,0 @@
-use noop_drop_strategy::NoOpDropStrategy;
-use try_drop::debugging::{ErrorsOnDrop, Fallible};
-use try_drop::drop_adapter::DropAdapter;
-
-fn main() {
-    let errors = DropAdapter(ErrorsOnDrop::<Fallible, _>::given(NoOpDropStrategy, NoOpDropStrategy));
-    println!("dropping errors now");
-    drop(errors);
-}
--- drop-strategies/noop/src/lib.rs
+++
@@ -1,7 +0,0 @@
-use try_drop::{Error, TryDropStrategy};
-
-pub struct NoOpDropStrategy;
-
-impl TryDropStrategy for NoOpDropStrategy {
-    fn handle_error(&self, _error: Error) {}
-}
