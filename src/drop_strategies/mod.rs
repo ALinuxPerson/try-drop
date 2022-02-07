1cd7e5d2f52028d703eebf958e0ef21541f2178c
ALinuxPerson <alinuxperson@gmail.com>
2022-02-07 13:24:03 +0800

    Add feature guarded imports

--- src/drop_strategies/mod.rs
+++ src/drop_strategies/mod.rs
@@ -15,3 +15,21 @@ pub mod panic;
 
 #[cfg(feature = "ds-write")]
 pub mod write;
+
+#[cfg(feature = "ds-abort")]
+pub use abort::AbortDropStrategy;
+
+#[cfg(feature = "ds-broadcast")]
+pub use broadcast::BroadcastDropStrategy;
+
+#[cfg(feature = "ds-exit")]
+pub use exit::ExitDropStrategy;
+
+#[cfg(feature = "ds-noop")]
+pub use noop::NoOpDropStrategy;
+
+#[cfg(feature = "ds-panic")]
+pub use panic::PanicDropStrategy;
+
+#[cfg(feature = "ds-write")]
+pub use write::WriteDropStrategy;
