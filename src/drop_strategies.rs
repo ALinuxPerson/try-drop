d39572649c8e440d3ac116f69d9d262813413b38
ALinuxPerson <alinuxperson@gmail.com>
2022-02-07 13:06:42 +0800

    Add panic drop strategy

--- src/drop_strategies.rs
+++ src/drop_strategies.rs
@@ -54,7 +54,45 @@ pub mod noop {
 }
 
 #[cfg(feature = "ds-panic")]
-pub mod panic {}
+pub mod panic {
+    use std::borrow::Cow;
+    use crate::{Error, TryDropStrategy};
+
+    #[cfg_attr(feature = "derives", derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash))]
+    pub struct PanicDropStrategy {
+        pub message: Cow<'static, str>,
+    }
+
+    impl PanicDropStrategy {
+        pub const fn new() -> Self {
+            Self::with_static_message("error occured when dropping an object")
+        }
+
+        pub fn with_message(message: impl Into<Cow<'static, str>>) -> Self {
+            Self { message: message.into() }
+        }
+
+        pub const fn with_static_message(message: &'static str) -> Self {
+            Self { message: Cow::Borrowed(message) }
+        }
+
+        pub const fn with_dynamic_message(message: String) -> Self {
+            Self { message: Cow::Owned(message) }
+        }
+    }
+
+    impl TryDropStrategy for PanicDropStrategy {
+        fn handle_error(&self, error: Error) {
+            Err(error).expect(&self.message)
+        }
+    }
+
+    impl Default for PanicDropStrategy {
+        fn default() -> Self {
+            Self::new()
+        }
+    }
+}
 
 #[cfg(feature = "ds-write")]
 pub mod write {}
