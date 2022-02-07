7c51c99ef1593b0210e71dee5295f9239c1b308f
ALinuxPerson <alinuxperson@gmail.com>
2022-02-07 14:58:57 +0800

    Add docs for no-op drop strategy

--- src/drop_strategies/noop.rs
+++ src/drop_strategies/noop.rs
@@ -1,5 +1,6 @@
 use crate::TryDropStrategy;
 
+/// A drop strategy which does nothing if a drop error occurs.
 #[cfg_attr(
     feature = "derives",
     derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Default)
