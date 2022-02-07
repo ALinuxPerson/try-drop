f8922759d2779d464ae53d175d622bb4ddc440c5
ALinuxPerson <alinuxperson@gmail.com>
2022-02-07 15:00:37 +0800

    Add docs for panic drop strategy and add `DEFAULT` constant for consistency

--- src/drop_strategies/panic.rs
+++ src/drop_strategies/panic.rs
@@ -2,31 +2,34 @@ use crate::{Error, TryDropStrategy};
 use std::borrow::Cow;
 use std::string::String;
 
+/// A drop strategy that panics with a message if a drop error occurs.
 #[cfg_attr(
     feature = "derives",
     derive(Debug, Clone, Ord, PartialOrd, Eq, PartialEq, Hash)
 )]
 pub struct PanicDropStrategy {
+    /// The message to panic with.
     pub message: Cow<'static, str>,
 }
 
 impl PanicDropStrategy {
-    pub const fn new() -> Self {
-        Self::with_static_message("error occured when dropping an object")
-    }
+    const DEFAULT: Self = Self::with_static_message("error occurred when dropping an object");
 
+    /// Creates a new panic drop strategy with the given message.
     pub fn with_message(message: impl Into<Cow<'static, str>>) -> Self {
         Self {
             message: message.into(),
         }
     }
 
+    /// Creates a new panic drop strategy with the given static message.
     pub const fn with_static_message(message: &'static str) -> Self {
         Self {
             message: Cow::Borrowed(message),
         }
     }
 
+    /// Creates a new panic drop strategy with the given string message.
     pub const fn with_dynamic_message(message: String) -> Self {
         Self {
             message: Cow::Owned(message),
@@ -42,6 +45,6 @@ impl TryDropStrategy for PanicDropStrategy {
 
 impl Default for PanicDropStrategy {
     fn default() -> Self {
-        Self::new()
+        Self::DEFAULT
     }
 }
