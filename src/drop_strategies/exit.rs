9183bbf9c37d9b02aa10a8b804a1a3eb26f8e5de
ALinuxPerson <alinuxperson@gmail.com>
2022-02-07 14:58:29 +0800

    Add docs for exit drop strategy

--- src/drop_strategies/exit.rs
+++ src/drop_strategies/exit.rs
@@ -1,17 +1,21 @@
 use crate::TryDropStrategy;
 use std::process;
 
+/// A drop strategy which exits the program with a specific exit code if the drop fails.
 #[cfg_attr(
     feature = "derives",
     derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash)
 )]
 pub struct ExitDropStrategy {
+    /// The exit code to use if the drop fails.
     pub exit_code: i32,
 }
 
 impl ExitDropStrategy {
+    /// The default exit drop strategy.
     pub const DEFAULT: Self = Self::new(1);
 
+    /// Create a new exit drop strategy.
     pub const fn new(exit_code: i32) -> Self {
         Self { exit_code }
     }
