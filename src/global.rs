3f04dc200ebfa9901cff7baea4ea0b1b0480db01
ALinuxPerson <alinuxperson@gmail.com>
2022-02-07 14:55:39 +0800

    Add docs for global

--- src/global.rs
+++ src/global.rs
@@ -1,3 +1,5 @@
+//! Manage the global try drop strategy.
+
 use crate::{FallibleTryDropStrategy, GlobalDynFallibleTryDropStrategy};
 use anyhow::Error;
 use once_cell::sync::OnceCell;
@@ -6,6 +8,8 @@ use std::boxed::Box;
 
 static DROP_STRATEGY: OnceCell<RwLock<Box<dyn GlobalDynFallibleTryDropStrategy>>> = OnceCell::new();
 
+/// The global try drop strategy. This doesn't store anything, it just provides an interface
+/// to the global fallback try drop strategy, stored in a `static`.
 #[cfg_attr(
     feature = "derives",
     derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash)
@@ -26,18 +30,22 @@ fn drop_strategy() -> &'static RwLock<Box<dyn GlobalDynFallibleTryDropStrategy>>
     )
 }
 
+/// Get a reference to the global try drop strategy.
 pub fn read() -> RwLockReadGuard<'static, Box<dyn GlobalDynFallibleTryDropStrategy>> {
     drop_strategy().read()
 }
 
+/// Get a mutable reference to the global try drop strategy.
 pub fn write() -> RwLockWriteGuard<'static, Box<dyn GlobalDynFallibleTryDropStrategy>> {
     drop_strategy().write()
 }
 
+/// Install a new global try drop strategy.
 pub fn install(drop_strategy: impl GlobalDynFallibleTryDropStrategy) {
     install_dyn(Box::new(drop_strategy))
 }
 
+/// Install a new global try drop strategy. Needs to be a dynamic trait object.
 pub fn install_dyn(drop_strategy: Box<dyn GlobalDynFallibleTryDropStrategy>) {
     match DROP_STRATEGY.get() {
         Some(global_drop_strategy) => *global_drop_strategy.write() = drop_strategy,
