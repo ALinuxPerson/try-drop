dcaeedea9dd94321526e38aedec66839f1e59b37
ALinuxPerson <alinuxperson@gmail.com>
2022-02-07 13:02:54 +0800

    Add abort drop strategy

--- src/drop_strategies.rs
+++ src/drop_strategies.rs
@@ -1,5 +1,16 @@
 #[cfg(feature = "ds-abort")]
-pub mod abort {}
+pub mod abort {
+    use std::process;
+
+    #[cfg_attr(feature = "derives", derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Default))]
+    pub struct AbortDropStrategy;
+
+    impl TryDropStrategy for AbortDropStrategy {
+        fn handle_error(&self, _error: crate::Error) {
+            process::abort()
+        }
+    }
+}
 
 #[cfg(feature = "ds-broadcast")]
 pub mod broadcast {}
