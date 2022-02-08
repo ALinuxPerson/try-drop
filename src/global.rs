3d9617ec98aa14e51ea88474a3ba4e2253f6bf55
ALinuxPerson <alinuxperson@gmail.com>
2022-02-08 17:43:58 +0800

    Use write drop strategy if it's available for global

--- src/global.rs
+++ src/global.rs
@@ -1,12 +1,61 @@
 //! Manage the global try drop strategy.
 
+#[cfg(feature = "ds-write")]
+mod drop_strategy {
+    use std::boxed::Box;
+    use once_cell::sync::Lazy;
+    use parking_lot::RwLock;
+    use crate::drop_strategies::WriteDropStrategy;
+    use crate::GlobalDynFallibleTryDropStrategy;
+
+    static DROP_STRATEGY: Lazy<RwLock<Box<dyn GlobalDynFallibleTryDropStrategy>>> = Lazy::new(|| {
+        let mut strategy = WriteDropStrategy::stderr();
+        strategy.prelude("error: ");
+        RwLock::new(Box::new(strategy))
+    });
+
+    pub fn drop_strategy() -> &'static RwLock<Box<dyn GlobalDynFallibleTryDropStrategy>> {
+        &DROP_STRATEGY
+    }
+
+    /// Install a new global try drop strategy. Needs to be a dynamic trait object.
+    pub fn install_dyn(strategy: Box<dyn GlobalDynFallibleTryDropStrategy>) {
+        *drop_strategy().write() = strategy
+    }
+}
+
+#[cfg(not(feature = "ds-write"))]
+mod drop_strategy {
+    use std::boxed::Box;
+    use once_cell::sync::OnceCell;
+    use parking_lot::RwLock;
+    use crate::GlobalDynFallibleTryDropStrategy;
+
+    static DROP_STRATEGY: OnceCell<RwLock<Box<dyn GlobalDynFallibleTryDropStrategy>>> = OnceCell::new();
+
+    pub fn drop_strategy() -> &'static RwLock<Box<dyn GlobalDynFallibleTryDropStrategy>> {
+        DROP_STRATEGY.get().expect(
+            "the global drop strategy is not initialized; initialize it with `global::install()`",
+        )
+    }
+
+    /// Install a new global try drop strategy. Needs to be a dynamic trait object.
+    pub fn install_dyn(drop_strategy: Box<dyn GlobalDynFallibleTryDropStrategy>) {
+        match DROP_STRATEGY.get() {
+            Some(global_drop_strategy) => *global_drop_strategy.write() = drop_strategy,
+            None => {
+                let _ = DROP_STRATEGY.set(RwLock::new(drop_strategy));
+            }
+        }
+    }
+}
+
+pub use drop_strategy::install_dyn;
+use drop_strategy::drop_strategy;
 use crate::{FallibleTryDropStrategy, GlobalDynFallibleTryDropStrategy};
 use anyhow::Error;
-use once_cell::sync::OnceCell;
-use parking_lot::{RwLock, RwLockReadGuard, RwLockWriteGuard};
+use parking_lot::{RwLockReadGuard, RwLockWriteGuard};
 use std::boxed::Box;
-
-static DROP_STRATEGY: OnceCell<RwLock<Box<dyn GlobalDynFallibleTryDropStrategy>>> = OnceCell::new();
 
 /// The global try drop strategy. This doesn't store anything, it just provides an interface
 /// to the global fallback try drop strategy, stored in a `static`.
@@ -24,12 +73,6 @@ impl FallibleTryDropStrategy for GlobalTryDropStrategyHandler {
     }
 }
 
-fn drop_strategy() -> &'static RwLock<Box<dyn GlobalDynFallibleTryDropStrategy>> {
-    DROP_STRATEGY.get().expect(
-        "the global drop strategy is not initialized; initialize it with `global::install()`",
-    )
-}
-
 /// Get a reference to the global try drop strategy.
 pub fn read() -> RwLockReadGuard<'static, Box<dyn GlobalDynFallibleTryDropStrategy>> {
     drop_strategy().read()
@@ -43,14 +86,4 @@ pub fn write() -> RwLockWriteGuard<'static, Box<dyn GlobalDynFallibleTryDropStra
 /// Install a new global try drop strategy.
 pub fn install(drop_strategy: impl GlobalDynFallibleTryDropStrategy) {
     install_dyn(Box::new(drop_strategy))
-}
-
-/// Install a new global try drop strategy. Needs to be a dynamic trait object.
-pub fn install_dyn(drop_strategy: Box<dyn GlobalDynFallibleTryDropStrategy>) {
-    match DROP_STRATEGY.get() {
-        Some(global_drop_strategy) => *global_drop_strategy.write() = drop_strategy,
-        None => {
-            let _ = DROP_STRATEGY.set(RwLock::new(drop_strategy));
-        }
-    }
 }
