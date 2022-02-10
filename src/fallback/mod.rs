83c58626d4461845cc0580c945c2efbf2d6eebe6
ALinuxPerson <alinuxperson@gmail.com>
2022-02-10 11:39:45 +0800

    Add new fallback thread local

--- src/fallback/mod.rs
+++ src/fallback/mod.rs
@@ -6,6 +6,9 @@ pub mod global;
 #[cfg(feature = "thread-local")]
 pub mod thread_local;
 
+#[cfg(feature = "thread-local")]
+pub mod new_thread_local;
+
 #[cfg(all(feature = "global", feature = "thread-local"))]
 pub mod shim;
 
