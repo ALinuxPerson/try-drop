79bbbfcb7f318774615a9bd6f7d6c0d953ea3f0b
ALinuxPerson <alinuxperson@gmail.com>
2022-02-11 09:56:01 +0800

    Remove unused imports and make it no_std compatible

--- src/fallback/mod.rs
+++ src/fallback/mod.rs
@@ -11,42 +11,7 @@ mod private {
 }
 
 use crate::on_uninit::OnUninit;
-use crate::{DynFallibleTryDropStrategy, FallibleTryDropStrategy, TryDropStrategy};
-use anyhow::Error;
-use std::sync::atomic::AtomicBool;
-
-// /// An error handler for drop strategies. If a struct implements [`TryDropStrategy`], it can also
-// /// be used as a [`FallbackTryDropStrategy`]. This **cannot** fail.
-// pub trait FallbackTryDropStrategy {
-//     /// Handle an error in a drop strategy.
-//     fn handle_error_in_strategy(&self, error: anyhow::Error);
-// }
-//
-// impl<TDS: TryDropStrategy> FallbackTryDropStrategy for TDS {
-//     fn handle_error_in_strategy(&self, error: Error) {
-//         self.handle_error(error)
-//     }
-// }
-//
-// #[cfg(feature = "global")]
-// #[cfg(not(feature = "downcast-rs"))]
-// pub trait GlobalFallbackTryDropStrategy: crate::ThreadSafe + FallbackTryDropStrategy {}
-//
-// /// Signifies that a type is try drop strategy which can be used as a fallback, and can also be used
-// /// as the global fallback try drop strategy.
-// #[cfg(feature = "global")]
-// #[cfg(feature = "downcast-rs")]
-// pub trait GlobalFallbackTryDropStrategy:
-//     crate::ThreadSafe + downcast_rs::DowncastSync + FallbackTryDropStrategy
-// {
-// }
-//
-// #[cfg(feature = "global")]
-// #[cfg(feature = "downcast-rs")]
-// downcast_rs::impl_downcast!(sync GlobalFallbackTryDropStrategy);
-//
-// #[cfg(feature = "global")]
-// impl<T: crate::ThreadSafe + FallbackTryDropStrategy> GlobalFallbackTryDropStrategy for T {}
+use core::sync::atomic::AtomicBool;
 
 pub trait OnUninitFallback: private::Sealed {
     type ExtraData;
