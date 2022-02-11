1b711b90f92f6fe8eb745adf455f0bc81cced6e9
ALinuxPerson <alinuxperson@gmail.com>
2022-02-11 10:53:59 +0800

    Use on thread local

--- src/handlers/primary/thread_local.rs
+++ src/handlers/primary/thread_local.rs
@@ -2,11 +2,12 @@
 use std::boxed::Box;
 use std::cell::RefCell;
 
-use crate::on_uninit::{ErrorOnUninit, OnUninit, PanicOnUninit, UseDefaultOnUninit};
+use crate::on_uninit::{ErrorOnUninit, FlagOnUninit, OnUninit, PanicOnUninit, UseDefaultOnUninit};
 use crate::uninit_error::UninitializedError;
 use crate::utils::NotSendNotSync;
 use crate::{DynFallibleTryDropStrategy, FallibleTryDropStrategy};
 use std::marker::PhantomData;
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::thread_local;
 
 thread_local! {
@@ -32,34 +33,67 @@ pub type DefaultOnUninit = UseDefaultOnUninit;
 feature = "derives",
 derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Default)
 )]
-pub struct ThreadLocalPrimaryTryDropStrategy<OU: OnUninit = PanicOnUninit>(PhantomData<(OU, NotSendNotSync)>);
+pub struct ThreadLocalPrimaryTryDropStrategy<OU: OnUninit = PanicOnUninit> {
+    extra_data: OU::ExtraData,
+    _marker: PhantomData<(OU, NotSendNotSync)>,
+}
 
 impl ThreadLocalPrimaryTryDropStrategy<DefaultOnUninit> {
-    pub const DEFAULT: Self = Self(PhantomData);
+    pub const DEFAULT: Self = Self { extra_data: (), _marker: PhantomData };
 }
 
 impl ThreadLocalPrimaryTryDropStrategy<ErrorOnUninit> {
+    /// See [`Self::on_uninit_error`].
+    pub const ERROR_ON_UNINIT: Self = Self::on_uninit_error();
+
     /// Create a new interface to the thread local drop strategy. If the thread local drop strategy
     /// is not initialized, this will error.
     pub const fn on_uninit_error() -> Self {
-        Self(PhantomData)
+        Self { extra_data: (), _marker: PhantomData }
     }
 }
 
 impl ThreadLocalPrimaryTryDropStrategy<PanicOnUninit> {
+    /// See [`Self::on_uninit_panic`].
+    pub const PANIC_ON_UNINIT: Self = Self::on_uninit_panic();
+
     /// Create a new interface to the thread local drop strategy. If the thread local drop strategy
     /// is not initialized, this will panic.
     pub const fn on_uninit_panic() -> Self {
-        Self(PhantomData)
+        Self { extra_data: (), _marker: PhantomData }
     }
 }
 
 #[cfg(feature = "ds-write")]
 impl ThreadLocalPrimaryTryDropStrategy<UseDefaultOnUninit> {
+    /// See [`Self::on_uninit_use_default`].
+    pub const USE_DEFAULT_ON_UNINIT: Self = Self::on_uninit_use_default();
+
     /// Create a new interface to the thread local drop strategy. If the thread local drop strategy
     /// is not initialized, this will set it to the default drop strategy.
     pub const fn on_uninit_use_default() -> Self {
-        Self(PhantomData)
+        Self { extra_data: (), _marker: PhantomData }
+    }
+}
+
+impl ThreadLocalPrimaryTryDropStrategy<FlagOnUninit> {
+    /// See [`Self::on_uninit_flag`].
+    pub const FLAG_ON_UNINIT: Self = Self::on_uninit_flag();
+
+    /// Create a new interface to the thread local drop strategy. If the thread local drop strategy
+    /// is not initialized, this will set an internal flag stating that the drop failed.
+    pub const fn on_uninit_flag() -> Self {
+        Self { extra_data: AtomicBool::new(false), _marker: PhantomData }
+    }
+
+    /// Check if the last drop failed due to the primary thread local drop strategy not being
+    /// initialized.
+    pub fn last_drop_failed(&self) -> bool {
+        self.extra_data.load(Ordering::Acquire)
+    }
+
+    fn set_last_drop_failed(&self, value: bool) {
+        self.extra_data.store(value, Ordering::Release)
     }
 }
 
@@ -87,6 +121,20 @@ impl FallibleTryDropStrategy for ThreadLocalPrimaryTryDropStrategy<UseDefaultOnU
 
     fn try_handle_error(&self, error: crate::Error) -> Result<(), Self::Error> {
         read_or_default(|strategy| strategy.dyn_try_handle_error(error))
+    }
+}
+
+impl FallibleTryDropStrategy for ThreadLocalPrimaryTryDropStrategy<FlagOnUninit> {
+    type Error = anyhow::Error;
+
+    fn try_handle_error(&self, error: crate::Error) -> Result<(), Self::Error> {
+        let (last_drop_failed, ret) = match try_read(|s| s.dyn_try_handle_error(error)) {
+            Ok(Ok(())) => (false, Ok(())),
+            Ok(Err(error)) => (false, Err(error)),
+            Err(error) => (true, Err(error.into())),
+        };
+        self.set_last_drop_failed(last_drop_failed);
+        ret
     }
 }
 
