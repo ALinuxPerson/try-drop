bb4a39ab78c14b32cd625c65cdd4b86ae2a9ffa0
ALinuxPerson <alinuxperson@gmail.com>
2022-02-11 10:44:38 +0800

    Use `OU::ExtraData` on the actual trait

--- src/on_uninit.rs
+++ src/on_uninit.rs
@@ -11,30 +11,50 @@ mod use_default {
     /// Use the default drop strategy if uninitialized
     pub enum UseDefaultOnUninit {}
 
-    impl OnUninit for UseDefaultOnUninit {}
+    impl OnUninit for UseDefaultOnUninit {
+        type ExtraData = ();
+    }
     impl private::Sealed for UseDefaultOnUninit {}
 }
 
+use core::sync::atomic::AtomicBool;
 #[cfg(any(feature = "ds-write", feature = "ds-panic"))]
 pub use use_default::*;
 
 /// What to do when the global, thread local, or shim drop strategies is uninitialized.
-pub trait OnUninit: private::Sealed {}
+pub trait OnUninit: private::Sealed {
+    /// Any extra data that this type may neee.
+    type ExtraData;
+}
 
 /// Just error on the drop strategy if uninitialized.
 pub enum ErrorOnUninit {}
 
-impl OnUninit for ErrorOnUninit {}
+impl OnUninit for ErrorOnUninit {
+    type ExtraData = ();
+}
 impl private::Sealed for ErrorOnUninit {}
 
 /// Panic on the drop strategy if uninitialized.
 pub enum PanicOnUninit {}
 
-impl OnUninit for PanicOnUninit {}
+impl OnUninit for PanicOnUninit {
+    type ExtraData = ();
+}
 impl private::Sealed for PanicOnUninit {}
 
 /// Does nothing if uninitialized.
 pub enum DoNothingOnUninit {}
 
-impl OnUninit for DoNothingOnUninit {}
+impl OnUninit for DoNothingOnUninit {
+    type ExtraData = ();
+}
 impl private::Sealed for DoNothingOnUninit {}
+
+/// Sets an internal flag if uninitialized.
+pub enum FlagOnUninit {}
+
+impl OnUninit for FlagOnUninit {
+    type ExtraData = AtomicBool;
+}
+impl private::Sealed for FlagOnUninit {}
