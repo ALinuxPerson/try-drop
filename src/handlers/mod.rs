7db14248e4c40512510e70ea0bd9a461296d324a
ALinuxPerson <alinuxperson@gmail.com>
2022-02-11 10:09:29 +0800

    Add helper methods for handlers

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,3 +1,46 @@
+use std::boxed::Box;
+use crate::{DynFallibleTryDropStrategy, GlobalDynFallibleTryDropStrategy, GlobalTryDropStrategy, TryDropStrategy};
+
 pub mod primary;
 pub mod fallback;
 mod common;
+
+pub fn install_global_handlers(
+    primary: impl GlobalDynFallibleTryDropStrategy,
+    fallback: impl GlobalTryDropStrategy,
+) {
+    install_global_handlers_dyn(primary, fallback)
+}
+
+pub fn install_global_handlers_dyn(
+    primary: Box<dyn GlobalDynFallibleTryDropStrategy>,
+    fallback: Box<dyn GlobalTryDropStrategy>,
+) {
+    primary::global::install_dyn(primary);
+    fallback::global::install_dyn(fallback);
+}
+
+pub fn uninstall_globally() {
+    primary::global::uninstall();
+    fallback::global::uninstall();
+}
+
+pub fn install_thread_local_handlers(
+    primary: impl DynFallibleTryDropStrategy,
+    fallback: impl TryDropStrategy,
+) {
+    install_thread_local_handlers_dyn(primary, fallback)
+}
+
+pub fn install_thread_local_handlers_dyn(
+    primary: Box<dyn DynFallibleTryDropStrategy>,
+    fallback: Box<dyn TryDropStrategy>,
+) {
+    primary::thread_local::install_dyn(primary);
+    fallback::thread_local::install_dyn(fallback);
+}
+
+pub fn uninstall_for_thread() {
+    primary::thread_local::uninstall();
+    fallback::thread_local::uninstall();
+}
