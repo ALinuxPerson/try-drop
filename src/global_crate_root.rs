1e95c374fac428365d2486701cafd2c70d959004
ALinuxPerson <alinuxperson@gmail.com>
2022-02-11 13:51:15 +0800

    Use `static`s

--- src/global_crate_root.rs
+++ src/global_crate_root.rs
@@ -7,8 +7,7 @@ pub use crate::handlers::{
     uninstall_globally,
     uninstall_for_thread
 };
-use crate::handlers::PrimaryDropStrategy;
-use crate::handlers::FallbackDropStrategy;
+use crate::handlers::{DEFAULT_FALLBACK_DROP_STRATEGY, DEFAULT_PRIMARY_DROP_STRATEGY, PrimaryDropStrategy, FallbackDropStrategy};
 
 impl<TD: ImpureTryDrop> PureTryDrop for TD {
     type Error = TD::Error;
@@ -16,11 +15,11 @@ impl<TD: ImpureTryDrop> PureTryDrop for TD {
     type TryDropStrategy = PrimaryDropStrategy;
 
     fn fallback_try_drop_strategy(&self) -> &Self::FallbackTryDropStrategy {
-        &FallbackDropStrategy::DEFAULT
+        &DEFAULT_FALLBACK_DROP_STRATEGY
     }
 
     fn try_drop_strategy(&self) -> &Self::TryDropStrategy {
-        &PrimaryDropStrategy::DEFAULT
+        &DEFAULT_PRIMARY_DROP_STRATEGY
     }
 
     unsafe fn try_drop(&mut self) -> Result<(), Self::Error> {
