4bf1e6ae09461bd81b7b6a0efbc8bdff55f09443
ALinuxPerson <alinuxperson@gmail.com>
2022-02-11 11:53:56 +0800

    Feature gate `UseDefaultOnUninitShim`

--- src/handlers/shim.rs
+++ src/handlers/shim.rs
@@ -7,6 +7,41 @@ use crate::on_uninit::OnUninit;
 mod private {
     pub trait Sealed {}
 }
+#[cfg(any(feature = "ds-write", feature = "ds-panic"))]
+mod use_default {
+    use std::marker::PhantomData;
+    use once_cell::unsync::Lazy;
+    use crate::handlers::shim::OnUninitShim;
+    use super::private;
+
+    pub struct UseDefaultOnUninitShim<H: Handler>(PhantomData<H>);
+
+    #[cfg(feature = "ds-write")]
+    impl OnUninitShim for UseDefaultOnUninitShim<PrimaryHandler> {
+        type ExtraData = Lazy<crate::drop_strategies::WriteDropStrategy<std::io::Stderr>>;
+    }
+
+    #[cfg(feature = "ds-panic")]
+    impl OnUninitShim for UseDefaultOnUninitShim<FallbackHandler> {
+        type ExtraData = Lazy<crate::drop_strategies::PanicDropStrategy>;
+    }
+
+    impl<H: Handler> private::Sealed for UseDefaultOnUninitShim<H> {}
+
+    pub trait Handler: private::Sealed {}
+
+    pub enum FallbackHandler {}
+    impl Handler for FallbackHandler {}
+    impl private::Sealed for FallbackHandler {}
+
+    pub enum PrimaryHandler {}
+    impl Handler for PrimaryHandler {}
+    impl private::Sealed for PrimaryHandler {}
+
+}
+
+#[cfg(any(feature = "ds-write", feature = "ds-panic"))]
+pub use use_default::*;
 
 pub trait OnUninitShim: private::Sealed {
     type ExtraData;
@@ -16,24 +51,3 @@ impl<T: OnUninit> OnUninitShim for T {
     type ExtraData = T::ExtraData;
 }
 impl<T: OnUninit> private::Sealed for T {}
-
-pub struct UseDefaultOnUninitShim<H: Handler>(PhantomData<H>);
-
-impl OnUninitShim for UseDefaultOnUninitShim<PrimaryHandler> {
-    type ExtraData = Lazy<WriteDropStrategy<io::Stderr>>;
-}
-impl OnUninitShim for UseDefaultOnUninitShim<FallbackHandler> {
-    type ExtraData = Lazy<PanicDropStrategy>;
-}
-
-impl<H: Handler> private::Sealed for UseDefaultOnUninitShim<H> {}
-
-pub trait Handler: private::Sealed {}
-
-pub enum FallbackHandler {}
-impl Handler for FallbackHandler {}
-impl private::Sealed for FallbackHandler {}
-
-pub enum PrimaryHandler {}
-impl Handler for PrimaryHandler {}
-impl private::Sealed for PrimaryHandler {}
