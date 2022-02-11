b0d2743acd410b6d7c878efdc67bc9c07d844a4d
ALinuxPerson <alinuxperson@gmail.com>
2022-02-11 11:41:06 +0800

    Implement fallback shim

---
+++ src/handlers/fallback/shim.rs
@@ -0,0 +1,118 @@
+use std::io;
+use std::sync::atomic::{AtomicBool, Ordering};
+use anyhow::Error;
+use once_cell::unsync::Lazy;
+use crate::on_uninit::{DoNothingOnUninit, ErrorOnUninit, FlagOnUninit, PanicOnUninit};
+use crate::{FallibleTryDropStrategy, TryDropStrategy};
+use crate::drop_strategies::broadcast::ArcError;
+use crate::drop_strategies::{PanicDropStrategy, WriteDropStrategy};
+use crate::handlers::fallback::global::GlobalFallbackDropStrategy;
+use crate::handlers::fallback::thread_local::ThreadLocalFallbackDropStrategy;
+use crate::handlers::shim::{OnUninitShim, FallbackHandler, UseDefaultOnUninitShim};
+
+pub struct ShimFallbackDropStrategy<OU: OnUninitShim> {
+    global: GlobalFallbackDropStrategy<FlagOnUninit>,
+    thread_local: ThreadLocalFallbackDropStrategy<FlagOnUninit>,
+    extra_data: OU::ExtraData,
+}
+
+impl ShimFallbackDropStrategy<PanicOnUninit> {
+    pub const PANIC_ON_UNINIT: Self = Self {
+        global: GlobalFallbackDropStrategy::FLAG_ON_UNINIT,
+        thread_local: ThreadLocalFallbackDropStrategy::FLAG_ON_UNINIT,
+        extra_data: (),
+    };
+
+    pub const fn on_uninit_panic() -> Self {
+        Self::PANIC_ON_UNINIT
+    }
+}
+
+impl ShimFallbackDropStrategy<DoNothingOnUninit> {
+    pub const DO_NOTHING_ON_UNINIT: Self = Self {
+        global: GlobalFallbackDropStrategy::FLAG_ON_UNINIT,
+        thread_local: ThreadLocalFallbackDropStrategy::FLAG_ON_UNINIT,
+        extra_data: (),
+    };
+
+    pub const fn on_uninit_do_nothing() -> Self {
+        Self::DO_NOTHING_ON_UNINIT
+    }
+}
+
+impl ShimFallbackDropStrategy<FlagOnUninit> {
+    pub const FLAG_ON_UNINIT: Self = Self {
+        global: GlobalFallbackDropStrategy::FLAG_ON_UNINIT,
+        thread_local: ThreadLocalFallbackDropStrategy::FLAG_ON_UNINIT,
+        extra_data: AtomicBool::new(false),
+    };
+
+    pub const fn flag_on_uninit() -> Self {
+        Self::FLAG_ON_UNINIT
+    }
+
+    pub fn last_drop_failed(&self) -> bool {
+        self.extra_data.load(Ordering::Acquire)
+    }
+
+    fn set_last_drop_failed(&self, value: bool) {
+        self.extra_data.store(value, Ordering::Release)
+    }
+}
+
+impl ShimFallbackDropStrategy<UseDefaultOnUninitShim<FallbackHandler>> {
+    pub const USE_DEFAULT_ON_UNINIT: Self = Self {
+        global: GlobalFallbackDropStrategy::FLAG_ON_UNINIT,
+        thread_local: ThreadLocalFallbackDropStrategy::FLAG_ON_UNINIT,
+        extra_data: Lazy::new(|| PanicDropStrategy::DEFAULT),
+    };
+
+    pub const fn use_default_on_uninit() -> Self {
+        Self::USE_DEFAULT_ON_UNINIT
+    }
+
+    fn cache(&self) -> &PanicDropStrategy {
+        &self.extra_data
+    }
+}
+
+impl<OU: OnUninitShim> ShimFallbackDropStrategy<OU> {
+    fn on_all_uninit(&self, error: anyhow::Error, f: impl FnOnce(ArcError)) {
+        let error = ArcError::new(error);
+        self.thread_local.handle_error(ArcError::clone(&error).into());
+
+        if self.global.last_drop_failed() {
+            self.thread_local.handle_error(ArcError::clone(&error).into());
+            f(error)
+        }
+    }
+}
+
+impl TryDropStrategy for ShimFallbackDropStrategy<PanicOnUninit> {
+    fn handle_error(&self, error: crate::Error) {
+        self.on_all_uninit(
+            error,
+            |error| panic!("neither the fallback thread local nor the fallback global drop strategies are initialized (but here's the drop error anyway: {error})")
+        )
+    }
+}
+
+impl TryDropStrategy for ShimFallbackDropStrategy<DoNothingOnUninit> {
+    fn handle_error(&self, error: Error) {
+        self.on_all_uninit(error, |_| ())
+    }
+}
+
+impl TryDropStrategy for ShimFallbackDropStrategy<FlagOnUninit> {
+    fn handle_error(&self, error: Error) {
+        let mut last_drop_failed = false;
+        self.on_all_uninit(error, |_| last_drop_failed = true);
+        self.set_last_drop_failed(last_drop_failed);
+    }
+}
+
+impl TryDropStrategy for ShimFallbackDropStrategy<UseDefaultOnUninitShim<FallbackHandler>> {
+    fn handle_error(&self, error: Error) {
+        self.on_all_uninit(error, |error| self.cache().handle_error(error.into()))
+    }
+}
