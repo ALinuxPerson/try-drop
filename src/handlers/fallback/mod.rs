a972dfc7aa66ce868b7d3f54300aa9183a693aec
ALinuxPerson <alinuxperson@gmail.com>
2022-02-11 11:41:29 +0800

    Add shim modules

--- src/handlers/fallback/mod.rs
+++ src/handlers/fallback/mod.rs
@@ -4,27 +4,12 @@ pub mod global;
 #[cfg(feature = "thread-local")]
 pub mod thread_local;
 
+#[cfg(all(feature = "global", feature = "thread-local"))]
+pub mod shim;
+
 mod private {
     pub trait Sealed {}
 }
 
 use crate::on_uninit::OnUninit;
 use core::sync::atomic::AtomicBool;
-
-pub trait OnUninitFallback: private::Sealed {
-    type ExtraData;
-}
-
-impl<OU: OnUninit> OnUninitFallback for OU {
-    type ExtraData = ();
-}
-
-impl<OU: OnUninit> private::Sealed for OU {}
-
-pub enum FlagOnUninit {}
-
-impl OnUninitFallback for FlagOnUninit {
-    type ExtraData = AtomicBool;
-}
-impl private::Sealed for FlagOnUninit {}
-
--- src/handlers/primary/mod.rs
+++ src/handlers/primary/mod.rs
@@ -3,3 +3,6 @@ pub mod global;
 
 #[cfg(feature = "thread-local")]
 pub mod thread_local;
+
+#[cfg(all(feature = "global", feature = "thread-local"))]
+pub mod shim;
