cebf6e937d815e6db61dbd7e90f9f343eac11942
ALinuxPerson <alinuxperson@gmail.com>
2022-02-12 19:05:37 +0800

    Add handy functions for thread local scope handler

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -56,6 +56,28 @@ pub(crate) mod fns {
         fallback::thread_local::install_dyn(fallback);
     }
 
+    /// This installs the primary and fallback thread local handlers for this scope.
+    #[cfg(feature = "thread-local")]
+    pub fn install_thread_local_handlers_for_this_scope(
+        primary: impl DynFallibleTryDropStrategy + 'static,
+        fallback: impl TryDropStrategy + 'static,
+    ) -> (primary::thread_local::ScopeGuard, fallback::thread_local::ScopeGuard) {
+        install_thread_local_handlers_for_this_scope_dyn(Box::new(primary), Box::new(fallback))
+    }
+
+    /// This installs the primary and fallback thread local handlers for this scope. Must be a
+    /// dynamic trait object.
+    #[cfg(feature = "thread-local")]
+    pub fn install_thread_local_handlers_for_this_scope_dyn(
+        primary: Box<dyn DynFallibleTryDropStrategy>,
+        fallback: Box<dyn TryDropStrategy>,
+    ) -> (primary::thread_local::ScopeGuard, fallback::thread_local::ScopeGuard) {
+        (
+            primary::thread_local::scope_dyn(primary),
+            fallback::thread_local::scope_dyn(fallback),
+        )
+    }
+
     /// This uninstalls the primary and fallback thread local handlers.
     #[cfg(feature = "thread-local")]
     pub fn uninstall_for_thread() {
