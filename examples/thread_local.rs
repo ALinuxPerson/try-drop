1c34aede439cb2c957e8a38b18a00d6745cd3ead
ALinuxPerson <alinuxperson@gmail.com>
2022-02-12 11:28:14 +0800

    Add `thread_local` example

---
+++ examples/thread_local.rs
@@ -0,0 +1,39 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+use std::thread;
+use try_drop::drop_strategies::{AdHocFallibleTryDropStrategy, AdHocTryDropStrategy};
+use try_drop::handlers::primary::shim::ShimPrimaryDropStrategy;
+use try_drop::PureTryDrop;
+use try_drop::test_utils::{ErrorsOnDrop, Fallible};
+
+fn main() {
+    println!("install thread local handlers from main thread");
+    let thread_local_fail = Rc::new(RefCell::new(false));
+    let tlf = Rc::clone(&thread_local_fail);
+    try_drop::install_thread_local_handlers(
+        AdHocFallibleTryDropStrategy(move |error| {
+            println!("from primary thread local handler: {error}");
+
+            if *tlf.borrow() {
+                println!("forcing failure");
+                anyhow::bail!("forced failure of primary thread local handler")
+            } else {
+                Ok(())
+            }
+        }),
+        AdHocTryDropStrategy(|error| println!("from fallback thread local handler: {error}")),
+    );
+    println!("drop, don't fail for primary thread local handler");
+    let thing = ErrorsOnDrop::<Fallible, _>::not_given().adapt();
+    drop(thing);
+
+    println!("drop, do fail for primary thread local handler");
+    *thread_local_fail.borrow_mut() = true;
+    let thing = ErrorsOnDrop::<Fallible, _>::not_given().adapt();
+    drop(thing);
+
+    thread::spawn(|| {
+        println!("start new thread with no thread handlers, should just write the error");
+        let _errors = ErrorsOnDrop::<Fallible, _>::not_given().adapt();
+    }).join().unwrap();
+}
