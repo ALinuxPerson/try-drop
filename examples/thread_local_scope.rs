8314f484e3f4b95fb35eb08fcc630939f68ef1c5
ALinuxPerson <alinuxperson@gmail.com>
2022-02-12 19:08:27 +0800

    Add thread local scope example

---
+++ examples/thread_local_scope.rs
@@ -0,0 +1,49 @@
+use try_drop::drop_strategies::AdHocTryDropStrategy;
+use try_drop::drop_strategies::unreachable::UnreachableDropStrategy;
+use try_drop::handlers::*;
+use try_drop::PureTryDrop;
+use try_drop::test_utils::{ErrorsOnDrop, Fallible};
+
+fn main() {
+    println!("install main thread local handler");
+    primary::thread_local::install(
+        AdHocTryDropStrategy(|error| println!("error from the main thread local: {error}"))
+    );
+    let thing = ErrorsOnDrop::<Fallible, _>::not_given().adapt();
+    println!("drop test from main thread local handler");
+    drop(thing);
+
+    {
+        println!("install first thread local scope");
+        let _guard = primary::thread_local::scope(AdHocTryDropStrategy(|error| println!("error from the first thread local scope: {error}")));
+
+        let thing = ErrorsOnDrop::<Fallible, _>::not_given().adapt();
+        println!("drop test from first thread local scope");
+        drop(thing)
+    }
+
+    println!("after the scope is dropped, we should be using the main thread local scope again");
+    let thing = ErrorsOnDrop::<Fallible, _>::not_given().adapt();
+    println!("drop test from exit of first thread local scope");
+    drop(thing);
+
+    {
+        println!("install second thread local scope");
+        let _guard = primary::thread_local::scope(AdHocTryDropStrategy(|error| println!("error from the second thread local scope: {error}")));
+
+        let thing = ErrorsOnDrop::<Fallible, _>::not_given().adapt();
+        println!("drop test from second thread local scope");
+        drop(thing)
+    }
+
+    {
+        println!("create nested scope");
+        let _guard = primary::thread_local::ScopeGuard::try_new(UnreachableDropStrategy::safe());
+
+        {
+            println!("nested scopes aren't allowed");
+            let error = primary::thread_local::ScopeGuard::try_new(UnreachableDropStrategy::safe()).unwrap_err();
+            println!("the error comes in a form of a `LockedError`: debug={error:?}, display={error}")
+        }
+    }
+}
