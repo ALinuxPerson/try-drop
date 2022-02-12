97612138fe95577801cf0f5f2f19726b91df63dc
ALinuxPerson <alinuxperson@gmail.com>
2022-02-12 21:11:28 +0800

    Add some tests to fallback thread local

--- src/handlers/fallback/thread_local/mod.rs
+++ src/handlers/fallback/thread_local/mod.rs
@@ -236,3 +236,88 @@ pub fn scope(strategy: impl TryDropStrategy + 'static) -> ScopeGuard {
 pub fn scope_dyn(strategy: Box<dyn TryDropStrategy>) -> ScopeGuard {
     ScopeGuard::new_dyn(strategy)
 }
+
+#[cfg(test)]
+mod tests {
+    use std::rc::Rc;
+    use anyhow::anyhow;
+    use crate::drop_strategies::{AdHocFallibleTryDropStrategy, IntoAdHocTryDropStrategy, NoOpDropStrategy};
+    use crate::handlers::primary;
+    use crate::PureTryDrop;
+    use crate::test_utils::{ErrorsOnDrop, Fallible};
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn test_panic_on_uninit() {
+        ThreadLocalFallbackHandler::on_uninit_panic().handle_error(anyhow!("test"))
+    }
+
+    #[test]
+    fn test_flag_on_uninit() {
+        let handler = ThreadLocalFallbackHandler::on_uninit_flag();
+        assert!(!handler.last_drop_failed(), "last drop error handle failed but we haven't dropped anything yet");
+        handler.handle_error(anyhow!("test"));
+        assert!(handler.last_drop_failed(), "last drop error handle didn't fail but we have dropped something while not initialized");
+        install(NoOpDropStrategy);
+        handler.handle_error(anyhow!("test"));
+        assert!(!handler.last_drop_failed(), "last drop error handle failed but we have initialized");
+    }
+
+    #[test]
+    fn test_install() {
+        let installed = Rc::new(RefCell::new(false));
+        let i = Rc::clone(&installed);
+        install((move |_| *i.borrow_mut() = true).into_adhoc_try_drop_strategy());
+        primary::thread_local::install(AdHocFallibleTryDropStrategy(|error| Err(error)));
+        drop(ErrorsOnDrop::<Fallible, _>::not_given().adapt());
+        assert!(*installed.borrow(), "install didn't install");
+    }
+
+    #[test]
+    fn test_install_dyn() {
+        let installed = Rc::new(RefCell::new(false));
+        let i = Rc::clone(&installed);
+        install_dyn(Box::new((move |_| *i.borrow_mut() = true).into_adhoc_try_drop_strategy()));
+        primary::thread_local::install(AdHocFallibleTryDropStrategy(|error| Err(error)));
+        drop(ErrorsOnDrop::<Fallible, _>::not_given().adapt());
+        assert!(*installed.borrow(), "install_dyn didn't install");
+    }
+
+    #[test]
+    #[should_panic(expected = "the thread local fallback handler is not initialized yet: UninitializedError(())")]
+    fn test_read_panics_on_uninit() {
+        read(|_| panic!("did not panic on uninit"))
+    }
+
+    #[test]
+    fn test_try_read_errors_on_uninit() {
+        try_read(|_| panic!("did not error on uninit")).expect_err("did not error on uninit");
+    }
+
+    #[test]
+    fn test_read_or_default() {
+        let mut executed = false;
+        read_or_default(|_| executed = true);
+        assert!(executed, "read_or_default didn't execute");
+    }
+
+    #[test]
+    #[should_panic(expected = "the thread local fallback handler is not initialized yet: UninitializedError(())")]
+    fn test_write_panics_on_uninit() {
+        write(|_| panic!("did not panic on uninit"))
+    }
+
+    #[test]
+    fn test_try_write_errors_on_uninit() {
+        try_write(|_| panic!("did not error on uninit")).expect_err("did not error on uninit");
+    }
+
+    #[test]
+    fn test_write_or_default() {
+        let mut executed = false;
+        write_or_default(|_| executed = true);
+        assert!(executed, "read_or_default didn't execute");
+    }
+    // todo: test uninstall, take, replace, replace_dyn, scope, scope_dyn
+}
