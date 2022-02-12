88eeb50b95e245580ea8fb5abe477d3e68350dd2
ALinuxPerson <alinuxperson@gmail.com>
2022-02-12 10:50:44 +0800

    Allow `panic` example to compile

--- examples/panic.rs
+++ examples/panic.rs
@@ -1,9 +1,9 @@
 use try_drop::drop_strategies::{AbortDropStrategy, PanicDropStrategy};
 use try_drop::test_utils::{ErrorsOnDrop, Random};
-use try_drop::DropAdapter;
+use try_drop::adapters::DropAdapter;
 
 fn main() {
-    try_drop::install(PanicDropStrategy::DEFAULT, AbortDropStrategy);
+    try_drop::install_global_handlers(PanicDropStrategy::DEFAULT, AbortDropStrategy);
     let errors = DropAdapter(ErrorsOnDrop::<Random, _>::not_given());
     println!("dropping now (will only error sometimes)");
     drop(errors);
