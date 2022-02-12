dee9ce9d7b0a3112ba8b001bf42981c7963373e4
ALinuxPerson <alinuxperson@gmail.com>
2022-02-12 10:03:08 +0800

    Add thread unsafe variants of adhoc mut

---
+++ src/drop_strategies/adhoc/fn_mut/thread_unsafe.rs
@@ -0,0 +1,58 @@
+use std::cell::RefCell;
+use anyhow::Error;
+use crate::{FallibleTryDropStrategy, TryDropStrategy};
+
+pub struct ThreadUnsafeAdHocMutDropStrategy<F: FnMut(crate::Error)>(pub RefCell<F>);
+
+impl<F: FnMut(crate::Error)> ThreadUnsafeAdHocMutDropStrategy<F> {
+    pub fn new(f: F) -> Self {
+        Self(RefCell::new(f))
+    }
+}
+
+impl<F: FnMut(crate::Error)> TryDropStrategy for ThreadUnsafeAdHocMutDropStrategy<F> {
+    fn handle_error(&self, error: crate::Error) {
+        self.0.borrow_mut()(error)
+    }
+}
+
+pub trait IntoThreadUnsafeFnMutDropStrategy: FnMut(crate::Error) + Sized {
+    fn into_drop_strategy(self) -> ThreadUnsafeAdHocMutDropStrategy<Self> {
+        ThreadUnsafeAdHocMutDropStrategy::new(self)
+    }
+}
+
+pub struct ThreadUnsafeAdHocMutFallibleDropStrategy<F, E>(pub RefCell<F>)
+where
+    F: FnMut(crate::Error) -> Result<(), E>,
+    E: Into<crate::Error>;
+
+impl<F, E> ThreadUnsafeAdHocMutFallibleDropStrategy<F, E>
+where
+    F: FnMut(crate::Error) -> Result<(), E>,
+    E: Into<crate::Error>,
+{
+    pub fn new(f: F) -> Self {
+        Self(RefCell::new(f))
+    }
+}
+
+impl<F, E> FallibleTryDropStrategy for ThreadUnsafeAdHocMutFallibleDropStrategy<F, E>
+    where
+        F: FnMut(crate::Error) -> Result<(), E>,
+        E: Into<crate::Error>
+{
+    type Error = E;
+
+    fn try_handle_error(&self, error: Error) -> Result<(), Self::Error> {
+        self.0.borrow_mut()(error)
+    }
+}
+
+pub trait IntoThreadUnsafeFnMutFallibleDropStrategy: FnMut(crate::Error) -> Result<(), Self::Error> + Sized {
+    type Error: Into<crate::Error>;
+
+    fn into_drop_strategy(self) -> ThreadUnsafeAdHocMutFallibleDropStrategy<Self, Self::Error> {
+        ThreadUnsafeAdHocMutFallibleDropStrategy::new(self)
+    }
+}
