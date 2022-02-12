630da1a82acbe00b313cd47ac60ea760751ca0e5
ALinuxPerson <alinuxperson@gmail.com>
2022-02-12 10:45:21 +0800

    Allow `abort` to compile

--- examples/abort.rs
+++ examples/abort.rs
@@ -1,9 +1,9 @@
-use try_drop::drop_strategies::AbortDropStrategy;
 use try_drop::test_utils::{ErrorsOnDrop, Fallible};
-use try_drop::DropAdapter;
+use try_drop::adapters::DropAdapter;
+use try_drop::drop_strategies::AbortDropStrategy;
 
 fn main() {
-    try_drop::install(AbortDropStrategy, AbortDropStrategy);
+    try_drop::install_global_handlers(AbortDropStrategy, AbortDropStrategy);
     let errors = DropAdapter(ErrorsOnDrop::<Fallible, _>::not_given());
     println!("dropping now");
     drop(errors);
