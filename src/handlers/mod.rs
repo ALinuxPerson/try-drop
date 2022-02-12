434f4e9ce8264b0650de6e1dc003cba474bbb093
ALinuxPerson <alinuxperson@gmail.com>
2022-02-12 11:39:19 +0800

    Add docs for module level `handlers` functions

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -10,6 +10,7 @@ pub(crate) mod fns {
     #[cfg(feature = "global")]
     use crate::{GlobalDynFallibleTryDropStrategy, GlobalTryDropStrategy};
 
+    /// This installs the primary and fallback global handlers.
     #[cfg(feature = "global")]
     pub fn install_global_handlers(
         primary: impl GlobalDynFallibleTryDropStrategy,
@@ -18,6 +19,7 @@ pub(crate) mod fns {
         install_global_handlers_dyn(Box::new(primary), Box::new(fallback))
     }
 
+    /// This installs the primary and fallback global handlers. Must be a dynamic trait object.
     #[cfg(feature = "global")]
     pub fn install_global_handlers_dyn(
         primary: Box<dyn GlobalDynFallibleTryDropStrategy>,
@@ -27,12 +29,14 @@ pub(crate) mod fns {
         fallback::global::install_dyn(fallback);
     }
 
+    /// This uninstalls the primary and fallback global handlers.
     #[cfg(feature = "global")]
     pub fn uninstall_globally() {
         primary::global::uninstall();
         fallback::global::uninstall();
     }
 
+    /// This installs the primary and fallback thread local handlers.
     #[cfg(feature = "thread-local")]
     pub fn install_thread_local_handlers(
         primary: impl DynFallibleTryDropStrategy + 'static,
@@ -41,6 +45,8 @@ pub(crate) mod fns {
         install_thread_local_handlers_dyn(Box::new(primary), Box::new(fallback))
     }
 
+    /// This installs the primary and fallback thread local handlers. Must be a dynamic trait
+    /// object.
     #[cfg(feature = "thread-local")]
     pub fn install_thread_local_handlers_dyn(
         primary: Box<dyn DynFallibleTryDropStrategy>,
@@ -50,6 +56,7 @@ pub(crate) mod fns {
         fallback::thread_local::install_dyn(fallback);
     }
 
+    /// This uninstalls the primary and fallback thread local handlers.
     #[cfg(feature = "thread-local")]
     pub fn uninstall_for_thread() {
         primary::thread_local::uninstall();
