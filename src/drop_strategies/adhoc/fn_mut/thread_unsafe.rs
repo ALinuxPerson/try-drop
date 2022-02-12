36b5f8a542b1b02aa844d6e3456dd88778dee105
ALinuxPerson <alinuxperson@gmail.com>
2022-02-12 19:35:07 +0800

    Add docs for thread unsafe adhoc mut

--- src/drop_strategies/adhoc/fn_mut/thread_unsafe.rs
+++ src/drop_strategies/adhoc/fn_mut/thread_unsafe.rs
@@ -2,6 +2,8 @@ use std::cell::RefCell;
 use anyhow::Error;
 use crate::{FallibleTryDropStrategy, TryDropStrategy};
 
+/// A drop strategy which uses a function to handle errors. This is less flexible than its thread
+/// safe counterpart however there is less overhead.
 #[cfg_attr(
     feature = "derives",
     derive(Debug, Clone, Ord, PartialOrd, Eq, PartialEq)
@@ -9,6 +11,7 @@ use crate::{FallibleTryDropStrategy, TryDropStrategy};
 pub struct ThreadUnsafeAdHocMutDropStrategy<F: FnMut(crate::Error)>(pub RefCell<F>);
 
 impl<F: FnMut(crate::Error)> ThreadUnsafeAdHocMutDropStrategy<F> {
+    /// Create a new thread unsafe adhoc mut drop strategy.
     pub fn new(f: F) -> Self {
         Self(RefCell::new(f))
     }
@@ -20,12 +23,16 @@ impl<F: FnMut(crate::Error)> TryDropStrategy for ThreadUnsafeAdHocMutDropStrateg
     }
 }
 
+/// Turn this type into a [`ThreadUnsafeAdHocMutDropStrategy`].
 pub trait IntoThreadUnsafeAdHocMutDropStrategy: FnMut(crate::Error) + Sized {
+    /// Turn this type into a [`ThreadUnsafeAdHocMutDropStrategy`].
     fn into_drop_strategy(self) -> ThreadUnsafeAdHocMutDropStrategy<Self> {
         ThreadUnsafeAdHocMutDropStrategy::new(self)
     }
 }
 
+/// A fallible drop strategy which uses a function to handle errors. This is less flexible than its
+/// thread safe counterpart however there is less overhead.
 #[cfg_attr(
     feature = "derives",
     derive(Debug, Clone, Ord, PartialOrd, Eq, PartialEq)
@@ -40,6 +47,7 @@ where
     F: FnMut(crate::Error) -> Result<(), E>,
     E: Into<crate::Error>,
 {
+    /// Create a new thread unsafe adhoc mut fallible drop strategy.
     pub fn new(f: F) -> Self {
         Self(RefCell::new(f))
     }
@@ -57,9 +65,12 @@ impl<F, E> FallibleTryDropStrategy for ThreadUnsafeAdHocMutFallibleDropStrategy<
     }
 }
 
+/// Turn this type into a [`ThreadUnsafeAdHocMutFallibleDropStrategy`].
 pub trait IntoThreadUnsafeAdHocMutFallibleDropStrategy: FnMut(crate::Error) -> Result<(), Self::Error> + Sized {
+    /// The error type which will be used.
     type Error: Into<crate::Error>;
 
+    /// Turn this type into a [`ThreadUnsafeAdHocMutFallibleDropStrategy`].
     fn into_drop_strategy(self) -> ThreadUnsafeAdHocMutFallibleDropStrategy<Self, Self::Error> {
         ThreadUnsafeAdHocMutFallibleDropStrategy::new(self)
     }
