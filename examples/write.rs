57054c1143cd18c2efe0d9a6554c6d96c9e99b25
ALinuxPerson <alinuxperson@gmail.com>
2022-02-12 10:52:13 +0800

    Allow `write` example to compile

--- examples/write.rs
+++ examples/write.rs
@@ -1,11 +1,11 @@
 use try_drop::drop_strategies::{PanicDropStrategy, WriteDropStrategy};
 use try_drop::test_utils::{ErrorsOnDrop, Random};
-use try_drop::DropAdapter;
+use try_drop::adapters::DropAdapter;
 
 fn main() {
     let mut strategy = WriteDropStrategy::stderr();
     strategy.prelude("error: ");
-    try_drop::install(strategy, PanicDropStrategy::DEFAULT);
+    try_drop::install_global_handlers(strategy, PanicDropStrategy::DEFAULT);
     let errors = DropAdapter(ErrorsOnDrop::<Random, _>::not_given());
     println!("dropping now (will only error randomly)");
     drop(errors);
