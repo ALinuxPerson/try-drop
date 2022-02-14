666d0a6605d512658549f2b9310bbb0d5048e1f2
ALinuxPerson <alinuxperson@gmail.com>
2022-02-14 08:59:30 +0800

    Implement generic scope guard for primary thread local

--- src/handlers/primary/thread_local/scope_guard.rs
+++ src/handlers/primary/thread_local/scope_guard.rs
@@ -1,77 +1,38 @@
 use super::*;
-use crate::handlers::common::NestedScopeError;
+use crate::handlers::common::{NestedScopeError, Primary};
 use crate::DynFallibleTryDropStrategy;
 use std::boxed::Box;
-use std::fmt;
+use std::thread::LocalKey;
+use crate::handlers::common::thread_local::scope_guard::{
+    ScopeGuard as GenericScopeGuard,
+    ScopeGuardDefinition,
+};
 
 thread_local! {
     static LOCKED: RefCell<bool> = RefCell::new(false);
 }
 
-/// This installs a thread local primary drop strategy for the current scope.
-pub struct ScopeGuard {
-    last_strategy: Option<Box<dyn DynFallibleTryDropStrategy>>,
-}
+impl ScopeGuardDefinition for Primary {
+    const DYN: &'static str = "DynFallibleTryDropStrategy";
+    type Dyn = Box<dyn DynFallibleTryDropStrategy>;
 
-impl ScopeGuard {
-    /// Create a new scope guard.
-    ///
-    /// # Panics
-    /// This panics if the scope guard was nested.
-    pub fn new(strategy: impl DynFallibleTryDropStrategy + 'static) -> Self {
-        Self::new_dyn(Box::new(strategy))
+    fn locked() -> &'static LocalKey<RefCell<bool>> {
+        &LOCKED
     }
 
-    /// Create a new scope guard. Must be a dynamic trait object.
-    ///
-    /// # Panics
-    /// This panics if the scope guard was nested
-    pub fn new_dyn(strategy: Box<dyn DynFallibleTryDropStrategy>) -> Self {
-        Self::try_new_dyn(strategy).expect("you cannot nest scope guards")
+    fn replace_dyn(new: Self::Dyn) -> Option<Self::Dyn> {
+        super::replace_dyn(new)
     }
 
-    /// Try and create a new scope guard.
-    ///
-    /// # Errors
-    /// This returns an error if the scope guard was nested.
-    pub fn try_new(
-        strategy: impl DynFallibleTryDropStrategy + 'static,
-    ) -> Result<Self, NestedScopeError> {
-        Self::try_new_dyn(Box::new(strategy))
-    }
-
-    /// Try and create a new scope guard. Must be a dynamic trait object.
-    ///
-    /// # Errors
-    /// This returns an error if the scope guard was nested.
-    pub fn try_new_dyn(
-        strategy: Box<dyn DynFallibleTryDropStrategy>,
-    ) -> Result<Self, NestedScopeError> {
-        if LOCKED.with(|cell| *cell.borrow()) {
-            Err(NestedScopeError(()))
-        } else {
-            LOCKED.with(|cell| *cell.borrow_mut() = true);
-            Ok(Self {
-                last_strategy: replace_dyn(strategy),
-            })
-        }
+    fn install_dyn(strategy: Self::Dyn) {
+        super::install_dyn(strategy)
     }
 }
 
-impl fmt::Debug for ScopeGuard {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        f.debug_struct("ScopeGuard")
-            .field("last_strategy", &"Option<Box<dyn DynFallibleDropStrategy>>")
-            .finish()
-    }
-}
+pub type ScopeGuard = GenericScopeGuard<Primary>;
 
-impl Drop for ScopeGuard {
-    fn drop(&mut self) {
-        if let Some(last_strategy) = self.last_strategy.take() {
-            install_dyn(last_strategy)
-        }
-
-        LOCKED.with(|cell| *cell.borrow_mut() = false)
+impl<D: DynFallibleTryDropStrategy + 'static> From<D> for Box<dyn DynFallibleTryDropStrategy> {
+    fn from(d: D) -> Self {
+        Box::new(d)
     }
 }
