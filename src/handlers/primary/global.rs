23acf1b8f6c4330036368626c37be883277e4435
ALinuxPerson <alinuxperson@gmail.com>
2022-02-14 20:34:09 +0800

    Move `global` into its own module

--- src/handlers/primary/global.rs
+++ src/handlers/primary/global.rs
@@ -10,122 +10,28 @@ use parking_lot::{
     MappedRwLockReadGuard, MappedRwLockWriteGuard, RwLock, RwLockReadGuard, RwLockWriteGuard,
 };
 use std::boxed::Box;
-use std::marker::PhantomData;
-use std::sync::atomic::AtomicBool;
-use crate::handlers::common::Primary;
+use std::convert;
 use crate::handlers::common::global::{DefaultGlobalDefinition, Global as GenericGlobal, GlobalDefinition};
+use crate::handlers::common::handler::CommonHandler;
+use crate::handlers::primary::{Abstracter, DefaultOnUninit};
+use crate::handlers::common::{
+    Primary,
+    Global as GlobalHandler,
+};
 
 #[cfg(feature = "ds-write")]
 use crate::handlers::on_uninit::UseDefaultOnUninit;
 
-/// The default global primary handler.
-pub static DEFAULT_GLOBAL_PRIMARY_DROP_STRATEGY: GlobalPrimaryHandler =
-    GlobalPrimaryHandler::DEFAULT;
-
-/// The default thing to do when the global primary primary handler is uninitialized, that is to
-/// panic.
-#[cfg(not(feature = "ds-write"))]
-pub type DefaultOnUninit = PanicOnUninit;
-
-/// The default thing to do when the global primary handler is uninitialized, that is to use the
-/// default. Note that this mutates the global primary handler.
-#[cfg(feature = "ds-write")]
-pub type DefaultOnUninit = UseDefaultOnUninit;
-
-/// The global primary handler. This doesn't store anything, it just provides an interface
-/// to the global primary handler, stored in a `static`.
-#[cfg_attr(
-    feature = "derives",
-    derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Default)
-)]
-pub struct GlobalPrimaryHandler<OU: OnUninit = DefaultOnUninit> {
-    extra_data: OU::ExtraData,
-    _on_uninit: PhantomData<OU>,
-}
-
-impl GlobalPrimaryHandler<DefaultOnUninit> {
-    /// The default global primary handler.
-    pub const DEFAULT: Self = Self {
-        extra_data: (),
-        _on_uninit: PhantomData,
-    };
-}
-
-impl GlobalPrimaryHandler<ErrorOnUninit> {
-    /// See [`Self::on_uninit_error`].
-    pub const ERROR_ON_UNINIT: Self = Self::on_uninit_error();
-
-    /// Get an interface to the global primary handler. If there is no global primary handler
-    /// initialized, this will error.
-    pub const fn on_uninit_error() -> Self {
-        Self {
-            extra_data: (),
-            _on_uninit: PhantomData,
-        }
-    }
-}
-
-impl GlobalPrimaryHandler<PanicOnUninit> {
-    /// See [`Self::on_uninit_panic`].
-    pub const PANIC_ON_UNINIT: Self = Self::on_uninit_panic();
-
-    /// Get an interface to the global primary handler. If there is no global primary handler
-    /// initialized, this will panic.
-    pub const fn on_uninit_panic() -> Self {
-        Self {
-            extra_data: (),
-            _on_uninit: PhantomData,
-        }
-    }
-}
-
-#[cfg(feature = "ds-write")]
-impl GlobalPrimaryHandler<UseDefaultOnUninit> {
-    /// See [`Self::on_uninit_use_default`].
-    pub const USE_DEFAULT_ON_UNINIT: Self = Self::on_uninit_use_default();
-
-    /// Get an interface to the global primary handler. If there is no global primary handler
-    /// initialized, this will set it to the default.
-    pub const fn on_uninit_use_default() -> Self {
-        Self {
-            extra_data: (),
-            _on_uninit: PhantomData,
-        }
-    }
-}
-
-impl GlobalPrimaryHandler<FlagOnUninit> {
-    /// See [`Self::on_uninit_flag`].
-    pub const FLAG_ON_UNINIT: Self = Self::on_uninit_flag();
-
-    /// Get an interface to the global primary handler. If there is no global primary handler
-    /// initialized, this will set an internal flag stating that the drop failed.
-    pub const fn on_uninit_flag() -> Self {
-        Self {
-            extra_data: AtomicBool::new(false),
-            _on_uninit: PhantomData,
-        }
-    }
-
-    /// Check if acquiring a reference to the global primary handler failed due to it not being
-    /// initialized.
-    pub fn last_drop_failed(&self) -> bool {
-        self.extra_data.load(LOAD_ORDERING)
-    }
-
-    fn set_last_drop_failed(&self, value: bool) {
-        self.extra_data.store(value, STORE_ORDERING)
-    }
-}
+pub type GlobalPrimaryHandler<OU = DefaultOnUninit> = CommonHandler<OU, GlobalHandler, Primary>;
+pub static DEFAULT_GLOBAL_PRIMARY_HANDLER: GlobalPrimaryHandler = GlobalPrimaryHandler::DEFAULT;
 
 impl FallibleTryDropStrategy for GlobalPrimaryHandler<ErrorOnUninit> {
     type Error = anyhow::Error;
 
     fn try_handle_error(&self, error: Error) -> Result<(), Self::Error> {
-        try_read()
-            .map(|strategy| strategy.dyn_try_handle_error(error))
+        Abstracter::<GlobalHandler>::try_read(|strategy| strategy.dyn_try_handle_error(error))
             .map_err(Into::into)
-            .and_then(std::convert::identity)
+            .and_then(convert::identity)
     }
 }
 
@@ -133,7 +39,8 @@ impl FallibleTryDropStrategy for GlobalPrimaryHandler<PanicOnUninit> {
     type Error = anyhow::Error;
 
     fn try_handle_error(&self, error: Error) -> Result<(), Self::Error> {
-        read().dyn_try_handle_error(error)
+        Abstracter::<GlobalHandler>::try_read(|strategy| strategy.dyn_try_handle_error(error))
+            .expect(<Primary as GlobalDefinition>::UNINITIALIZED_ERROR)
     }
 }
 
@@ -142,15 +49,15 @@ impl FallibleTryDropStrategy for GlobalPrimaryHandler<UseDefaultOnUninit> {
     type Error = anyhow::Error;
 
     fn try_handle_error(&self, error: Error) -> Result<(), Self::Error> {
-        read_or_default().dyn_try_handle_error(error)
+        Abstracter::<GlobalHandler>::read_or_default(|strategy| strategy.dyn_try_handle_error(error))
     }
 }
 
 impl FallibleTryDropStrategy for GlobalPrimaryHandler<FlagOnUninit> {
     type Error = anyhow::Error;
 
-    fn try_handle_error(&self, error: crate::Error) -> Result<(), Self::Error> {
-        let (last_drop_failed, ret) = match try_read().map(|s| s.dyn_try_handle_error(error)) {
+    fn try_handle_error(&self, error: Error) -> Result<(), Self::Error> {
+        let (last_drop_failed, ret) = match Abstracter::<GlobalHandler>::try_read(|s| s.dyn_try_handle_error(error)) {
             Ok(Ok(())) => (false, Ok(())),
             Ok(Err(error)) => (false, Err(error)),
             Err(error) => (true, Err(error.into())),
--- src/handlers/primary/handler.rs
+++ src/handlers/primary/handler.rs
@@ -9,21 +9,12 @@ use crate::handlers::common::proxy::TheGreatAbstracter;
 use crate::handlers::common::thread_local::ThreadLocalDefinition;
 use crate::handlers::on_uninit::{ErrorOnUninit, FlagOnUninit, OnUninit, PanicOnUninit, UseDefaultOnUninit};
 
-pub type GlobalPrimaryHandler<OU = DefaultOnUninit> = CommonHandler<OU, Global, Primary>;
+
 pub type ThreadLocalPrimaryHandler<OU = DefaultOnUninit> = CommonHandler<OU, ThreadLocal, Primary>;
 
-pub static DEFAULT_GLOBAL_PRIMARY_HANDLER: GlobalPrimaryHandler = GlobalPrimaryHandler::DEFAULT;
 pub static DEFAULT_THREAD_LOCAL_PRIMARY_HANDLER: ThreadLocalPrimaryHandler = ThreadLocalPrimaryHandler::DEFAULT;
 
-impl FallibleTryDropStrategy for GlobalPrimaryHandler<ErrorOnUninit> {
-    type Error = anyhow::Error;
 
-    fn try_handle_error(&self, error: Error) -> Result<(), Self::Error> {
-        Abstracter::<Global>::try_read(|strategy| strategy.dyn_try_handle_error(error))
-            .map_err(Into::into)
-            .and_then(convert::identity)
-    }
-}
 
 impl FallibleTryDropStrategy for ThreadLocalPrimaryHandler<ErrorOnUninit> {
     type Error = anyhow::Error;
@@ -35,14 +26,7 @@ impl FallibleTryDropStrategy for ThreadLocalPrimaryHandler<ErrorOnUninit> {
     }
 }
 
-impl FallibleTryDropStrategy for GlobalPrimaryHandler<PanicOnUninit> {
-    type Error = anyhow::Error;
 
-    fn try_handle_error(&self, error: Error) -> Result<(), Self::Error> {
-        Abstracter::<Global>::try_read(|strategy| strategy.dyn_try_handle_error(error))
-            .expect(<Primary as GlobalDefinition>::UNINITIALIZED_ERROR)
-    }
-}
 
 impl FallibleTryDropStrategy for ThreadLocalPrimaryHandler<PanicOnUninit> {
     type Error = anyhow::Error;
@@ -53,14 +37,7 @@ impl FallibleTryDropStrategy for ThreadLocalPrimaryHandler<PanicOnUninit> {
     }
 }
 
-#[cfg(feature = "ds-write")]
-impl FallibleTryDropStrategy for GlobalPrimaryHandler<UseDefaultOnUninit> {
-    type Error = anyhow::Error;
 
-    fn try_handle_error(&self, error: Error) -> Result<(), Self::Error> {
-        Abstracter::<Global>::read_or_default(|strategy| strategy.dyn_try_handle_error(error))
-    }
-}
 
 #[cfg(feature = "ds-write")]
 impl FallibleTryDropStrategy for ThreadLocalPrimaryHandler<UseDefaultOnUninit> {
@@ -71,19 +48,7 @@ impl FallibleTryDropStrategy for ThreadLocalPrimaryHandler<UseDefaultOnUninit> {
     }
 }
 
-impl FallibleTryDropStrategy for GlobalPrimaryHandler<FlagOnUninit> {
-    type Error = anyhow::Error;
 
-    fn try_handle_error(&self, error: Error) -> Result<(), Self::Error> {
-        let (last_drop_failed, ret) = match Abstracter::<Global>::try_read(|s| s.dyn_try_handle_error(error)) {
-            Ok(Ok(())) => (false, Ok(())),
-            Ok(Err(error)) => (false, Err(error)),
-            Err(error) => (true, Err(error.into())),
-        };
-        self.set_last_drop_failed(last_drop_failed);
-        ret
-    }
-}
 
 impl FallibleTryDropStrategy for ThreadLocalPrimaryHandler<FlagOnUninit> {
     type Error = anyhow::Error;
