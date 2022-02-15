d57df01e5e68e6626c077df0921c45c5a4f78ce2
ALinuxPerson <alinuxperson@gmail.com>
2022-02-15 08:34:51 +0800

    Rename `scope_type` to `handler`

--- src/handlers/fallback/macros.rs
+++ src/handlers/fallback/macros.rs
@@ -1,19 +1,19 @@
 macro_rules! impl_try_drop_strategy_for {
-    ($scope_type:ident where Scope: $scope:ident) => {
-        impl TryDropStrategy for $scope_type<PanicOnUninit> {
+    ($handler:ident where Scope: $scope:ident) => {
+        impl TryDropStrategy for $handler<PanicOnUninit> {
             fn handle_error(&self, error: crate::Error) {
                 Abstracter::<$scope>::read(|strategy| strategy.handle_error(error))
             }
         }
 
         #[cfg(feature = "ds-write")]
-        impl TryDropStrategy for $scope_type<UseDefaultOnUninit> {
+        impl TryDropStrategy for $handler<UseDefaultOnUninit> {
             fn handle_error(&self, error: Error) {
                 Abstracter::<$scope>::read_or_default(|strategy| strategy.handle_error(error))
             }
         }
 
-        impl TryDropStrategy for $scope_type<FlagOnUninit> {
+        impl TryDropStrategy for $handler<FlagOnUninit> {
             fn handle_error(&self, error: Error) {
                 if let Err(UninitializedError(())) =
                     Abstracter::<$scope>::try_read(|strategy| strategy.handle_error(error))
